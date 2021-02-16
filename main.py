9447409e87b04b950b7bf257343f807ba59b83a9
Akashdeepsingh98 <akashdeepsingh98@gmail.com>
2021-02-16 19:57:15 +0530

    working on internal node

--- main.py
+++ main.py
@@ -7,16 +7,28 @@ class InternalNode:
         self.data = []
 
     def insert(self, ele):
-        if len(self.data)<3*2-1:
-            if isinstance(self.data, LeafNode):
+
+        if len(self.data) < b*2-1:
+
+            if isinstance(self.data[0], LeafNode):
                 iinsert = 0
+                while iinsert+1<len(self.data) and self.data[iinsert+1]<=ele:
+                    iinsert += 2
                 
-                pass
+                if len(self.data[iinsert].data)==b:
+
+                    pass
+                else:
+                    pass
+
             else:
                 pass
+
         else:
-            if isinstance(self.data, LeafNode):
+            
+            if isinstance(self.data[0], LeafNode):
                 pass
+            
             else:
                 pass
         pass
@@ -26,6 +38,7 @@ class LeafNode:
     def __init__(self):
         self.data = []
         self.data.append(None)
+        self.parent = None
 
     def __repr__(self):
         return str(self.data)
@@ -40,23 +53,27 @@ class LeafNode:
 
             else:
                 # get a new leafnode and adjust stuff appropriately
-                il = ceil((b-1)/2)
-                self.data[-1] = LeafNode()
-                for i in range(il, len(self.data)-1):
-                    self.data[-1].data.insert(0, self.data[i])
-                newdata = []
-                for i in range(il):
-                    newdata.append(self.data[i])
-                newdata.append(self.data[-1])
-                self.data = newdata
-                self.data[:-1] = sorted(self.data[:-1], key=lambda x: x[0])
+                if self.parent==None:
+                    il = ceil((b-1)/2)
+                    self.data[-1] = LeafNode()
+                    for i in range(il, len(self.data)-1):
+                        self.data[-1].data.insert(0, self.data[i])
+                    newdata = []
+                    for i in range(il):
+                        newdata.append(self.data[i])
+                    newdata.append(self.data[-1])
+                    self.data = newdata
+                    self.data[:-1] = sorted(self.data[:-1], key=lambda x: x[0])
 
-                newInode = InternalNode()
-                newInode.data.append(self)
-                newInode.data.append(self.data[-1].data[0][0])
-                newInode.data.append(self.data[-1])
+                    newInode = InternalNode()
+                    newInode.data.append(self)
+                    newInode.data.append(self.data[-1].data[0][0])
+                    newInode.data.append(self.data[-1])
+                    self.parent = newInode
 
-                return newInode
+                    return newInode
+                else:
+                    pass
 
         else:
             self.data[present][1] += 1
